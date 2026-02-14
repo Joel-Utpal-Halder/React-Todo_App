85f87452b97cc34805cacc502d4415bbdcdad5b0
Joel Utpal Halder <ju.halder@gmail.com>
2026-02-14 07:45:18 +0600

    feat: update TodoList.jsx file

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,13 @@
 import Todo from "./Todo"; // ✅ Import the single-task component
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos }) => { // Receives the todos array from App.jsx.
   return (
     <ul>
       {/* Loop through each todo in the array */}
       {todos.map((todo) => (
-        // For each todo, render the Todo component
+        // Loops through todos with .map(). For each todo, render the Todo component. 
         // 'key={todo.id}' gives React a unique identifier for efficient rendering
-        <Todo key={todo.id} todo={todo} />
+        <Todo key={todo.id} todo={todo} /> // Passes each single todo object → to Todo.jsx.
       ))}
     </ul>
   );
